1459b3369f563527e81bd8d739577db59ecedea9
Max Hluhov <holosievo@gmail.com>
2018-08-23 16:31:38 +0300

    One more change

--- src/main/java/com/shildt/chapter01/Example.java
+++ src/main/java/com/shildt/chapter01/Example.java
@@ -7,5 +7,6 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("Простая программа на Java");
         System.out.println("Simple");
+        System.out.println("/************************/");
     }
 }
