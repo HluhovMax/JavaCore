ae4c2cfe28361f751d5af8b767b2e41ab57966f5
Max Hluhov <holosievo@gmail.com>
2018-08-23 16:47:34 +0300

    Second class adding

---
+++ src/main/java/com/shildt/chapter01/Example2.java
@@ -0,0 +1,10 @@
+package com.shildt.chapter01;
+
+/**
+ * Created by Max Hluhov on 23.08.2018.
+ */
+public class Example2 {
+    public static void main(String[] args) {
+        System.out.println("its work");
+    }
+}
