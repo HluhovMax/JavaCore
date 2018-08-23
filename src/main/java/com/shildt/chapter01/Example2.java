590c45a651054688d344ef73f695cffbca1c5cd4
Max Hluhov <holosievo@gmail.com>
2018-08-23 19:26:53 +0300

    Adding Example2

---
+++ src/main/java/com/shildt/chapter01/Example2.java
@@ -0,0 +1,18 @@
+package com.shildt.chapter01;
+
+/**
+ * Created by Max Hluhov on 23.08.2018.
+ */
+public class Example2 {
+    public static void main(String[] args) {
+        int num;
+        num = 100;
+        System.out.println("this is variable num: " + num);
+
+        num *= 2;
+
+        System.out.println("value of num  now equals");
+        System.out.println(num);
+
+    }
+}
