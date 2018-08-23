9987d61ef6c4368151239fd711602513d0803fc3
Max Hluhov <holosievo@gmail.com>
2018-08-23 18:27:51 +0300

    Deleted sout

--- src/main/java/com/shildt/chapter01/Example.java
+++ src/main/java/com/shildt/chapter01/Example.java
@@ -6,7 +6,5 @@ package com.shildt.chapter01;
 public class Example {
     public static void main(String[] args) {
         System.out.println("Простая программа на Java");
-        System.out.println("Simple");
-        System.out.println("/************************/");
     }
 }
