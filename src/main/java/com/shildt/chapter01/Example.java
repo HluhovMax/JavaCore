d6c655cbccecf53e73a6d4525dece062a99da4ae
Max Hluhov <holosievo@gmail.com>
2018-08-23 16:06:52 +0300

    Initial commit of the project

---
+++ src/main/java/com/shildt/chapter01/Example.java
@@ -0,0 +1,10 @@
+package com.shildt.chapter01;
+
+/**
+ * Created by Max Hluhov on 23.08.2018.
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java");
+    }
+}
