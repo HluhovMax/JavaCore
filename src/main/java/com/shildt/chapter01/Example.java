ff57ae7ed0386d51448bc82c4431b919098e3f94
Max Hluhov <holosievo@gmail.com>
2018-08-23 18:35:44 +0300

    Added ?

--- src/main/java/com/shildt/chapter01/Example.java
+++ src/main/java/com/shildt/chapter01/Example.java
@@ -6,5 +6,6 @@ package com.shildt.chapter01;
 public class Example {
     public static void main(String[] args) {
         System.out.println("Простая программа на Java");
+        System.out.println("?");
     }
 }
