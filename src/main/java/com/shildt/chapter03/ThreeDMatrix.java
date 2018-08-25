7433e7b6986734c3c991cb48221f08ac4a5921f3
Max Hluhov <holosievo@gmail.com>
2018-08-25 10:52:49 +0300

    next commit

---
+++ src/main/java/com/shildt/chapter03/ThreeDMatrix.java
@@ -0,0 +1,10 @@
+package com.shildt.chapter03;
+
+/**
+ * Created by Max Hluhov on 25.08.2018.
+ */
+public class ThreeDMatrix {
+    public static void main(String[] args) {
+        int threeD[][][] = new int[3][4][5];
+    }
+}
