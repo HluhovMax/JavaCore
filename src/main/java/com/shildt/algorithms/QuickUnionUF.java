13f5433c9fd12b1c3e096a3fddde21fc680aab68
Max Hluhov <holosievo@gmail.com>
2018-09-26 19:57:55 +0300

    'commit'

---
+++ src/main/java/com/shildt/algorithms/QuickUnionUF.java
@@ -0,0 +1,32 @@
+package com.shildt.algorithms;
+
+/**
+ * Created by Max Hluhov on 26.09.2018.
+ */
+public class QuickUnionUF {
+    private int [] id;
+
+    public QuickUnionUF(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    private int root(int i) {
+        while (i != id[i]) {
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean conected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+}
