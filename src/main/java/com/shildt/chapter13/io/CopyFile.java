f44ea1b286c220295a85bce8dcac076a9a4c1199
Max Hluhov <holosievo@gmail.com>
2018-10-06 16:00:59 +0300

    'c'

---
+++ src/main/java/com/shildt/chapter13/io/CopyFile.java
@@ -0,0 +1,40 @@
+package com.shildt.chapter13.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by Max Hluhov on 06.10.2018.
+ */
+public class CopyFile {
+    public static void main(String[] args) {
+        int i;
+        FileInputStream fileInputStream = null;
+        FileOutputStream fileOutputStream = null;
+        File f = new File("src\\main\\java\\com\\shildt\\chapter13\\io\\newFileCopy.txt");
+        try {
+            fileInputStream = new FileInputStream("src\\main\\java\\com\\shildt\\chapter13\\io\\newFile.txt");
+            fileOutputStream = new FileOutputStream(f);
+
+            do {
+                i = fileInputStream.read();
+                if (i != -1) fileOutputStream.write(i);
+            } while (i != -1);
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода-вывода " + e);
+        }finally {
+            try {
+                if (fileInputStream != null) fileInputStream.close();
+            } catch (IOException e) {
+                System.out.println("Ошибка закрытия файла ввода.");
+            }
+            try {
+                if (fileOutputStream != null) fileOutputStream.close();
+            } catch (IOException e) {
+                System.out.println("Ошибка закрытия файла вывода.");
+            }
+        }
+    }
+}
--- src/main/java/com/shildt/chapter13/io/ShowFile.java
+++ src/main/java/com/shildt/chapter13/io/ShowFile.java
@@ -1,7 +1,6 @@
 package com.shildt.chapter13.io;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
@@ -10,33 +9,24 @@ import java.io.IOException;
 public class ShowFile {
     public static void main(String[] args) {
         int i;
-        FileInputStream fis;
-
-        if (args.length != 1) {
-            System.out.println("Использование файла: newFile.txt");
-            return;
-        }
-        // попытка открыть файл
-        try {
-            fis = new FileInputStream(args[0]);
-        } catch (FileNotFoundException e) {
-            System.out.println("Невозможно открыть файл.");
-            return;
-        }
+        FileInputStream fis = null;
 
         try {
+            fis = new FileInputStream("src\\main\\java\\com\\shildt\\chapter13\\io\\newFile.txt");
             do {
                 i = fis.read();
                 if (i != -1) System.out.print((char) i);
             } while (i != -1);
         } catch (IOException e) {
-            System.out.println("Ошибка чтения из файла.");
+            System.out.println("Ошибка ввода-вывода: " + e);
+        }finally {
+            try {
+                if (fis != null) fis.close();
+            } catch (IOException e) {
+                System.out.println("Ошибка закрытия файла.");
+            }
         }
-        try {
-            fis.close();
-        } catch (IOException e) {
-            System.out.println("Ошибка закрытия файла.");
-        }
+
 
     }
 }
